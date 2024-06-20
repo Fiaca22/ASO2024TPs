e2d8c61febab354fcfb90b65bb8b3f59631b1dd6
Fiaca22 <alfonsobaridon@gmail.com>
2024-06-20 01:54:54 -0300

    TP5 parte 2

---
+++ TP5/nombre_edad_api.sh
@@ -0,0 +1,20 @@
+while true; do
+    # Solicitar al usuario un nombre
+    echo "Introduce un nombre:"
+    read nombre
+
+    # Hacer la solicitud a la API de Agify
+    response=$(curl -s "https://api.agify.io/?name=$nombre")
+
+    # Extraer la edad de la respuesta JSON utilizando jq
+    edad=$(echo $response | jq '.age')
+
+    # Comprobar si la edad es null
+    if [ "$edad" == "null" ]; then
+        echo "El nombre introducido no es válido o no se encuentra en la base de datos. Por favor, intenta con otro nombre."
+    else
+        # Mostrar la edad al usuario y salir del bucle
+        echo "La edad estimada para el nombre $nombre es $edad años."
+        break
+    fi
+done
